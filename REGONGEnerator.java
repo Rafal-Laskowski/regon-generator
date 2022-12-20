f38848143a9d86106feed4e64cfc84b6d9064ebf
Rafal-Laskowski <51403523+Rafal-Laskowski@users.noreply.github.com>
2022-12-20 18:19:02 +0100

    Update REGONGEnerator.java

--- REGONGEnerator.java
+++ REGONGEnerator.java
@@ -1,57 +1,63 @@
 import java.util.Random;
 
 public class REGONGenerator {
-  public static long generateShort() {
-    // Generate a random 9-digit REGON number
-    Random random = new Random();
-    long regon = 100000000L + random.nextInt(900000000);
+    public static String generateShort() {
+        Random random = new Random();
 
-    // Calculate the checksum
-    int checksum = 0;
-    for (int i = 0; i < 9; i++) {
-      int digit = (int)(regon % 10);
-      checksum += digit * (8 - i);
-      regon /= 10;
+        // Generate a random 5-digit number for the first half of the REGON
+        String firstHalf = String.format("%05d", random.nextInt(100000));
+
+        // Generate a random 3-digit number for the second half of the REGON
+        String secondHalf = String.format("%03d", random.nextInt(1000));
+
+        // Concatenate the two halves to form the raw REGON
+        String rawRegon = firstHalf + secondHalf;
+
+        // Calculate the checksum
+        int[] weights = {8, 9, 2, 3, 4, 5, 6, 7};
+        int sum = 0;
+        for (int i = 0; i < weights.length; i++) {
+            sum += weights[i] * Character.getNumericValue(rawRegon.charAt(i));
+        }
+
+        // Calculate the check digit
+        int checkDigit = sum % 11 % 10;
+
+        // Return the REGON with the check digit appended
+        return rawRegon + checkDigit;
     }
 
-    // Calculate the check digit
-    int checkDigit = checksum % 11;
-    if (checkDigit == 10) {
-      checkDigit = 0;
+    public static String generateLong() {
+        // Generate a random 9-digit REGON number
+        String firstHalf = generateShort();
+
+        // Generate a random 4-digit number for the second half of the REGON
+        Random random = new Random();
+        String secondHalf = String.format("%04d", random.nextInt(10000));
+
+        // Concatenate the two halves to form the raw REGON
+        String rawRegon = firstHalf + secondHalf;
+
+        // Calculate the checksum
+        int[] weights = {2, 4, 8, 5, 0, 9, 7, 3, 6, 1, 2, 4, 8};
+        int sum = 0;
+        for (int i = 0; i < weights.length; i++) {
+            sum += weights[i] * Character.getNumericValue(rawRegon.charAt(i));
+        }
+
+        // Calculate the check digit
+        int checkDigit = sum % 11 % 10;
+
+        // Return the REGON with the check digit appended
+        return rawRegon + checkDigit;
     }
 
-    // Append the check digit to the REGON number
-    return regon * 10 + checkDigit;
-  }
+    public static void main(String[] args) {
+        // Generate a short REGON number
+        String shortRegon = generateShort();
+        System.out.println("SHORT REGON: " + shortRegon);
 
-  public static long generateLong() {
-    // Generate a random 14-digit REGON number
-    Random random = new Random();
-    long regon = 10000000000000L + random.nextInt(900000000000000L);
-
-    // Calculate the checksum
-    int checksum = 0;
-    for (int i = 0; i < 14; i++) {
-      int digit = (int)(regon % 10);
-      if (i < 13) {
-        checksum += digit * (14 - i);
-      } else {
-        checksum += digit;
-      }
-      regon /= 10;
+        String longRegon = generateLong();
+        System.out.println("LONG REGON: " + longRegon);
     }
-
-    // Calculate the check digit
-    int checkDigit = checksum % 11;
-    if (checkDigit == 10) {
-      checkDigit = 0;
-    }
-
-    // Append the check digit to the REGON number
-    return regon * 10 + checkDigit;
-  }
-
-  public static void main(String[] args) {
-    // Generate a short REGON number
-    long shortRegon = generateShort();
-    System.out.println("Short REGON: " + shortRegon);
+}
